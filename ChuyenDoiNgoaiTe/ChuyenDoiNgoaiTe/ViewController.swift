fdec80c374895772618592f1216d3110db974720
mkig <matkinhig@gmail.com>
2019-07-22 00:52:55 +0700

    first commit

---
+++ ChuyenDoiNgoaiTe/ChuyenDoiNgoaiTe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChuyenDoiNgoaiTe
+//
+//  Created by  Lực Nguyễn on 7/22/19.
+//  Copyright © 2019  Lực Nguyễn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
